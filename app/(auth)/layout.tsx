0b316a30f889260545bb93b625d078c17e0ae2a2
Ivan Streltsov (Kharkiv) <i.streltsov@mobidev.biz>
2024-02-06 15:35:51 +0200

    initial commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'Threads-demo',
+  description: 'A Next.js 14 Meta Threads Application clone'
+}
+
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  return <></>
+}
